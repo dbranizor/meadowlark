945d50eaeba83df98c406bf47ce996cab2fd5dcc
dbra <derek.b@gmail.com>
2021-09-25 19:15:32 -0400

    S - bootup

---
+++ packages/santa-fe/src/bootup.js
@@ -0,0 +1,15 @@
+import Message from "./components/messages/message-state";
+const MessageCatalog = {
+  Message,
+};
+const localized = async function (components) {
+  const args = Array.prototype.slice.call(arguments);
+  await args.reduce(async (acc, curr) => {
+    const prev = await acc;
+    console.log("dingo localized called", curr);
+    await MessageCatalog[curr].init();
+    return prev;
+  }, Promise.resolve());
+};
+
+export { localized };
