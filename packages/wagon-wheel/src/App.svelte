2a47ff119c01691a20bf5b2e074e0d7bc407ec89
Derek Branizor <meadowlark@dereks-mbp.myfiosgateway.com>
2021-12-18 10:23:10 -0500

    Added Tailwind Initial Form

--- packages/wagon-wheel/src/App.svelte
+++ packages/wagon-wheel/src/App.svelte
@@ -1,203 +1,37 @@
 <script>
-  import { onMount } from "svelte";
-  import SvelteTable from "svelte-table";
+  import Messages from "../../santa-fe/src/components/messages/Messages.svelte";
 
-  let dbstate = {};
-  let rows = [];
-  let hash = ""
-  onMount(async () => {
-    let response;
-    try {
-      response = await fetch(
-        "https://localhost/central-park/dev/status?group_id=meadowlark",
-        {
-          method: "GET",
-          mode: "cors",
-          credentials: "same-origin",
-        }
-      );
-    } catch (error) {
-      throw new Error(error);
-    }
-    dbstate = await response.json();
-    rows = dbstate.data.messages;
-    if(dbstate.data.merkle.hash){
-      hash = dbstate.data.merkle.hash;
-    }
-
-  });
-
-  const columns = [
-    {
-      key: "column",
-      title: "Column",
-      value: (v) => v.column,
-      sortable: true,
-      filterOptions: (rows) => {
-        // use first letter of first_name to generate filter
-        let letrs = {};
-        rows.forEach((row) => {
-          let letr = row.column.charAt(0);
-          if (letrs[letr] === undefined)
-            letrs[letr] = {
-              name: `${letr.toUpperCase()}`,
-              value: letr.toLowerCase(),
-            };
-        });
-        // fix order
-        letrs = Object.entries(letrs)
-          .sort()
-          .reduce((o, [k, v]) => ((o[k] = v), o), {});
-        return Object.values(letrs);
-      },
-      filterValue: (v) => v.column.charAt(0).toLowerCase(),
-    },
-    {
-      key: "dataset",
-      title: "Dataset",
-      value: (v) => v.dataset,
-      sortable: true,
-      filterOptions: (rows) => {
-        // use first letter of first_name to generate filter
-        let letrs = {};
-        rows.forEach((row) => {
-          let letr = row.dataset.charAt(0);
-          if (letrs[letr] === undefined)
-            letrs[letr] = {
-              name: `${letr.toUpperCase()}`,
-              value: letr.toLowerCase(),
-            };
-        });
-        // fix order
-        letrs = Object.entries(letrs)
-          .sort()
-          .reduce((o, [k, v]) => ((o[k] = v), o), {});
-        return Object.values(letrs);
-      },
-      filterValue: (v) => v.dataset.charAt(0).toLowerCase(),
-    },
-    {
-      key: "group_id",
-      title: "Group ID",
-      value: (v) => v.group_id,
-      sortable: true,
-      filterOptions: (rows) => {
-        // use first letter of first_name to generate filter
-        let letrs = {};
-        rows.forEach((row) => {
-          let letr = row.group_id.charAt(0);
-          if (letrs[letr] === undefined)
-            letrs[letr] = {
-              name: `${letr.toUpperCase()}`,
-              value: letr.toLowerCase(),
-            };
-        });
-        // fix order
-        letrs = Object.entries(letrs)
-          .sort()
-          .reduce((o, [k, v]) => ((o[k] = v), o), {});
-        return Object.values(letrs);
-      },
-      filterValue: (v) => v.group_id.charAt(0).toLowerCase(),
-    },
-    {
-      key: "row",
-      title: "Row",
-      value: (v) => v.row,
-      sortable: true,
-      filterOptions: (rows) => {
-        // use first letter of first_name to generate filter
-        let letrs = {};
-        rows.forEach((row) => {
-          let letr = row.row.charAt(0);
-          if (letrs[letr] === undefined)
-            letrs[letr] = {
-              name: `${letr.toUpperCase()}`,
-              value: letr.toLowerCase(),
-            };
-        });
-        // fix order
-        letrs = Object.entries(letrs)
-          .sort()
-          .reduce((o, [k, v]) => ((o[k] = v), o), {});
-        return Object.values(letrs);
-      },
-      filterValue: (v) => v.row.charAt(0).toLowerCase(),
-    },
-    {
-      key: "timestamp",
-      title: "TS",
-      value: (v) => v.timestamp,
-      sortable: true,
-      filterOptions: (rows) => {
-        // use first letter of first_name to generate filter
-        let letrs = {};
-        rows.forEach((row) => {
-          let letr = row.timestamp.charAt(0);
-          if (letrs[letr] === undefined)
-            letrs[letr] = {
-              name: `${letr.toUpperCase()}`,
-              value: letr.toLowerCase(),
-            };
-        });
-        // fix order
-        letrs = Object.entries(letrs)
-          .sort()
-          .reduce((o, [k, v]) => ((o[k] = v), o), {});
-        return Object.values(letrs);
-      },
-      filterValue: (v) => v.timestamp.charAt(0).toLowerCase(),
-    },
-    {
-      key: "value",
-      title: "Value",
-      value: (v) => v.value,
-      sortable: true,
-      filterOptions: (rows) => {
-        // use first letter of first_name to generate filter
-        let letrs = {};
-        rows.forEach((row) => {
-          let letr = row.value.charAt(0);
-          if (letrs[letr] === undefined)
-            letrs[letr] = {
-              name: `${letr.toUpperCase()}`,
-              value: letr.toLowerCase(),
-            };
-        });
-        // fix order
-        letrs = Object.entries(letrs)
-          .sort()
-          .reduce((o, [k, v]) => ((o[k] = v), o), {});
-        return Object.values(letrs);
-      },
-      filterValue: (v) => v.value.charAt(0).toLowerCase(),
-    },
-  ];
+  export let showMessages = false;
+  let columnName, dataType, tableName;
 </script>
 
-<main>
-  <h1>Wagon Wheel {hash}</h1>
-  <SvelteTable {columns} {rows} />
-</main>
+<div class="w-full h-full flex items-center justify-center">
+  <div class="max-w-md">
+    <div class="flex flex-col">
+      <div class="mr-auto text-large font-semibold">Table Name</div>
+      <input type="text" />
+    </div>
+    <div class="flex-row">
+      <label for="">Field Name <span class="required">*</span> </label>
+      <input type="text" />
+    </div>
+    <div class="flex-row">
+      <label for="">Field Data Type</label>
+      <input type="text" />
+    </div>
+    <div class="flex-row flex">
+      <button class="ml-auto px-2 py-1 bg-green-500">Add Column</button>
+      <button class="px-2 py-1 bg-blue-500">Create Table</button>
+      <button class="px-2 py-1 bg-transparent border-red-500 border">
+        Cancel
+      </button>
+    </div>
+  </div>
+</div>
+
+{#if showMessages}
+  <Messages />
+{/if}
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
 </style>
---
+++ packages/wagon-wheel/src/Messages.svelte
@@ -0,0 +1,204 @@
+<script>
+    import { onMount } from "svelte";
+    import SvelteTable from "svelte-table";
+  
+    let dbstate = {};
+    let rows = [];
+    let hash = ""
+    onMount(async () => {
+      let response;
+      try {
+        response = await fetch(
+          "https://localhost/central-park/dev/status?group_id=meadowlark",
+          {
+            method: "GET",
+            mode: "cors",
+            credentials: "same-origin",
+          }
+        );
+      } catch (error) {
+        throw new Error(error);
+      }
+      dbstate = await response.json();
+      rows = dbstate.data.messages;
+      if(dbstate.data.merkle.hash){
+        hash = dbstate.data.merkle.hash;
+      }
+  
+    });
+  
+    const columns = [
+      {
+        key: "column",
+        title: "Column",
+        value: (v) => v.column,
+        sortable: true,
+        filterOptions: (rows) => {
+          // use first letter of first_name to generate filter
+          let letrs = {};
+          rows.forEach((row) => {
+            let letr = row.column.charAt(0);
+            if (letrs[letr] === undefined)
+              letrs[letr] = {
+                name: `${letr.toUpperCase()}`,
+                value: letr.toLowerCase(),
+              };
+          });
+          // fix order
+          letrs = Object.entries(letrs)
+            .sort()
+            .reduce((o, [k, v]) => ((o[k] = v), o), {});
+          return Object.values(letrs);
+        },
+        filterValue: (v) => v.column.charAt(0).toLowerCase(),
+      },
+      {
+        key: "dataset",
+        title: "Dataset",
+        value: (v) => v.dataset,
+        sortable: true,
+        filterOptions: (rows) => {
+          // use first letter of first_name to generate filter
+          let letrs = {};
+          rows.forEach((row) => {
+            let letr = row.dataset.charAt(0);
+            if (letrs[letr] === undefined)
+              letrs[letr] = {
+                name: `${letr.toUpperCase()}`,
+                value: letr.toLowerCase(),
+              };
+          });
+          // fix order
+          letrs = Object.entries(letrs)
+            .sort()
+            .reduce((o, [k, v]) => ((o[k] = v), o), {});
+          return Object.values(letrs);
+        },
+        filterValue: (v) => v.dataset.charAt(0).toLowerCase(),
+      },
+      {
+        key: "group_id",
+        title: "Group ID",
+        value: (v) => v.group_id,
+        sortable: true,
+        filterOptions: (rows) => {
+          // use first letter of first_name to generate filter
+          let letrs = {};
+          rows.forEach((row) => {
+            let letr = row.group_id.charAt(0);
+            if (letrs[letr] === undefined)
+              letrs[letr] = {
+                name: `${letr.toUpperCase()}`,
+                value: letr.toLowerCase(),
+              };
+          });
+          // fix order
+          letrs = Object.entries(letrs)
+            .sort()
+            .reduce((o, [k, v]) => ((o[k] = v), o), {});
+          return Object.values(letrs);
+        },
+        filterValue: (v) => v.group_id.charAt(0).toLowerCase(),
+      },
+      {
+        key: "row",
+        title: "Row",
+        value: (v) => v.row,
+        sortable: true,
+        filterOptions: (rows) => {
+          // use first letter of first_name to generate filter
+          let letrs = {};
+          rows.forEach((row) => {
+            let letr = row.row.charAt(0);
+            if (letrs[letr] === undefined)
+              letrs[letr] = {
+                name: `${letr.toUpperCase()}`,
+                value: letr.toLowerCase(),
+              };
+          });
+          // fix order
+          letrs = Object.entries(letrs)
+            .sort()
+            .reduce((o, [k, v]) => ((o[k] = v), o), {});
+          return Object.values(letrs);
+        },
+        filterValue: (v) => v.row.charAt(0).toLowerCase(),
+      },
+      {
+        key: "timestamp",
+        title: "TS",
+        value: (v) => v.timestamp,
+        sortable: true,
+        filterOptions: (rows) => {
+          // use first letter of first_name to generate filter
+          let letrs = {};
+          rows.forEach((row) => {
+            let letr = row.timestamp.charAt(0);
+            if (letrs[letr] === undefined)
+              letrs[letr] = {
+                name: `${letr.toUpperCase()}`,
+                value: letr.toLowerCase(),
+              };
+          });
+          // fix order
+          letrs = Object.entries(letrs)
+            .sort()
+            .reduce((o, [k, v]) => ((o[k] = v), o), {});
+          return Object.values(letrs);
+        },
+        filterValue: (v) => v.timestamp.charAt(0).toLowerCase(),
+      },
+      {
+        key: "value",
+        title: "Value",
+        value: (v) => v.value,
+        sortable: true,
+        filterOptions: (rows) => {
+          // use first letter of first_name to generate filter
+          let letrs = {};
+          rows.forEach((row) => {
+            let letr = row.value.charAt(0);
+            if (letrs[letr] === undefined)
+              letrs[letr] = {
+                name: `${letr.toUpperCase()}`,
+                value: letr.toLowerCase(),
+              };
+          });
+          // fix order
+          letrs = Object.entries(letrs)
+            .sort()
+            .reduce((o, [k, v]) => ((o[k] = v), o), {});
+          return Object.values(letrs);
+        },
+        filterValue: (v) => v.value.charAt(0).toLowerCase(),
+      },
+    ];
+  </script>
+  
+  <main>
+    <h1>Wagon Wheel {hash}</h1>
+    <SvelteTable {columns} {rows} />
+  </main>
+  
+  <style>
+    main {
+      text-align: center;
+      padding: 1em;
+      max-width: 240px;
+      margin: 0 auto;
+    }
+  
+    h1 {
+      color: #ff3e00;
+      text-transform: uppercase;
+      font-size: 4em;
+      font-weight: 100;
+    }
+  
+    @media (min-width: 640px) {
+      main {
+        max-width: none;
+      }
+    }
+  </style>
+  
